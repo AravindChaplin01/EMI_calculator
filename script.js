1189373372c0708c1aff9670a6ad36720a97e6af
AravindChaplin01 <138550845+AravindChaplin01@users.noreply.github.com>
2023-12-18 11:33:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculateEMI() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var interestRate = parseFloat(document.getElementById("interest").value);
+    var tenure = parseFloat(document.getElementById("tenure").value);
+
+    var monthlyInterest = interestRate / (12 * 100);
+    var totalPayments = tenure * 12;
+
+    var emi = (principal * monthlyInterest * Math.pow(1 + monthlyInterest, totalPayments)) / (Math.pow(1 + monthlyInterest, totalPayments) - 1);
+
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "Your Monthly EMI: $" + emi.toFixed(2);
+}
+￼Enter
