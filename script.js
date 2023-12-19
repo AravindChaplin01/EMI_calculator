7eff5e02642422e9a82004f2a8442308e33db0ec
AravindChaplin01 <138550845+AravindChaplin01@users.noreply.github.com>
2023-12-19 12:47:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function calculateEMI() {
     resultElement.innerHTML = "Your Monthly EMI: ₹" + emi.toFixed(2);
 }
 
+
+function displayTable(principal, monthlyInterest, totalPayments) {
+    var tableHTML = "<h2>Payment Schedule</h2><table><tr><th>Payment #</th><th>Principal</th><th>Interest</th><th>Total Payment</th><th>Remaining Balance</th></tr>";
+
+    var remainingBalance = principal;
+
+    for (var i = 1; i <= totalPayments; i++) {
+        var interestPayment = remainingBalance * monthlyInterest;
+        var principalPayment = emi - interestPayment;
+        remainingBalance -= principalPayment;
+
+        tableHTML += "<tr><td>" + i + "</td><td>$" + principalPayment.toFixed(2) + "</td><td>$" + interestPayment.toFixed(2) +
+                     "</td><td>$" + emi.toFixed(2) + "</td><td>$" + remainingBalance.toFixed(2) + "</td></tr>";
+    }
+
+    tableHTML += "</table>";
+
+    var tableElement = document.getElementById("payment-table");
+    tableElement.innerHTML = tableHTML;
+}
